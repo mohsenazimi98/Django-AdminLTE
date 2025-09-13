64fb277ddb6eb34106f35de45d8dcc11949fc22b
mohsen azimi <azimimohsen06@gmail.com>
2025-09-13 19:53:33 +0330

    update url's app

--- dashboard/app01/urls.py
+++ dashboard/app01/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import app01
+from .views import *
 
 urlpatterns = [
     path("", app01, name="app01"),
+    path("app01_1", app01_1, name="app01_1"), #newline
 ]

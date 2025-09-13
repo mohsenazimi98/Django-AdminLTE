96f66c4a288206370ba08217a7ad73156edd3ab8
mohsen azimi <azimimohsen06@gmail.com>
2025-09-13 19:48:10 +0330

    update view's app

--- dashboard/app01/views.py
+++ dashboard/app01/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from utils.menu import register_menu
+from utils.menu_ids import APP_IDS
 
-# Create your views here.
+@login_required
+@register_menu(label="آپ 01", app_id="app_01")
+def app01(request):
+    context = {}
+    return render(request, "app01/app01.html", context)

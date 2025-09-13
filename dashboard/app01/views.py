5fa882197e692f1b776b5898d88f753b0ea2d7af
mohsen azimi <azimimohsen06@gmail.com>
2025-09-13 19:52:09 +0330

    update view for add submenu

--- dashboard/app01/views.py
+++ dashboard/app01/views.py
@@ -8,3 +8,10 @@ from utils.menu_ids import APP_IDS
 def app01(request):
     context = {}
     return render(request, "app01/app01.html", context)
+
+
+@login_required
+@register_menu(label="آپ 01_1", parent="app_01")
+def app01_1(request):
+    context = {}
+    return render(request, "app01/app01_1.html", context)
